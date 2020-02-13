62d687f50218b2df07c7433cc5d88a9317b463b0
Logon Aluno <logonaluno@local.com>
2020-02-13 19:28:08 -0300

    projeto negociacoes

---
+++ js/app/models/Negociacao.js
@@ -0,0 +1,34 @@
+class Negociacao {
+    
+    constructor(data, quantidade, valor) {
+        
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        //Para não alterar diretamente
+        Object.freeze(this);
+    }
+
+    //metódos 
+    
+    //Quando usa o get ele sabe que estou chamando a função e não a variável
+    get volume() {
+        
+        return this._quantidade * this._valor;
+    }
+    
+    get data() {
+        
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        
+        return this._quantidade;
+    }
+    
+    get valor() {
+        
+        return this._valor;
+    }
+}
