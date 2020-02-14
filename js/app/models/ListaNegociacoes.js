5a02629c982734c4402ed2ba5cf847f0dd212c30
Jaqueline Silva de Oliveira <joliveira@acesso.digital>
2020-02-14 16:19:48 -0300

    incluindo funcao apagar negociacoes

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,17 +1,17 @@
 class ListaNegociacoes {
-    
-    constructor() {
-        
-        this._negociacoes = [];
-    }
-    
-    adiciona(negociacao) {
-        
-        this._negociacoes.push(negociacao);
-    }
-    
-    get negociacoes() {
-        
-        return [].concat(this._negociacoes);
-    }
+  constructor() {
+    this._negociacoes = [];
+  }
+
+  adiciona(negociacao) {
+    this._negociacoes.push(negociacao);
+  }
+
+  esvazia() {
+    this._negociacoes = [];
+  }
+
+  get negociacoes() {
+    return [].concat(this._negociacoes);
+  }
 }
